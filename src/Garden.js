5b4116a599967deec4e0db5b9e31ff6ea6c45c23
Yaroslav Karandashev <yarix7@gmail.com>
2014-04-17 16:54:55 +0400

    Update Garden.js

--- src/Garden.js
+++ src/Garden.js
@@ -7,16 +7,41 @@
 Garden = function() {
   this.actors = [];
   this.turnips = [];
+  this.eventDispatcher = null;
 };
 
 Garden.prototype.callTheActor = function() {
   var actor = Actor.factory();
+  actor.setGarden(this);
+  actor.setEventDispatcher(this.eventDispatcher);
   this.actors.push(actor);
   return actor;
 };
 
+Garden.prototype.setEventDispatcher = function(eventDispatcher) {
+  this.eventDispatcher = eventDispatcher;
+};
+
 Garden.prototype.callTheBestActor = function() {
-  return this.callTheActor();
+  var actorTemplate = this.pullActorWithHighestForce();
+  if (!actorTemplate) {
+      return null;
+  }
+  var actor = this.callTheActor();
+  actor.name = actorTemplate.name;
+  actor.force = actorTemplate.force;
+  return actor;
+};
+
+Garden.prototype.pullActorWithHighestForce = function() {
+  var currentBestActorIndex = 0;
+  this.actorsDictionary.forEach(function(value, index) {
+    var currentBestActor = this.actorsDictionary[currentBestActorIndex];
+    if (currentBestActor == undefined || currentBestActor.force < value.force) {
+        currentBestActorIndex = index;
+    }
+  }.bind(this));
+  return this.actorsDictionary.splice(currentBestActorIndex, currentBestActorIndex+1)[0];
 };
 
 Garden.prototype.getTurnipSeed = function() {
@@ -28,14 +53,14 @@ Garden.prototype.getTurnipSeed = function() {
 Garden.prototype.pullTheTurnip = function(turnip) {
   this.turnips.forEach(function(sTurnip, index){
     if (sTurnip == turnip) {
-      this.turnips.splice(index, i);
+      this.turnips.splice(index, index+1);
     }
   }.bind(this));
 };
 
-Garden.prototype.actorsDictionary = [{name: "Дедк", force: 1},
-                           {name: "Бабк", force: 0.5},
-                           {name: "Внучк", force: 0.3},
-                           {name: "Жучк", force: 0.2},
-                           {name: "Кошк", force: 0.1},
-                           {name: "Мышк", force: 0.01}];
+Garden.prototype.actorsDictionary = [{name: "Grandpa", force: 1},
+                           {name: "Grandma", force: 0.5},
+                           {name: "Granddaughter", force: 0.3},
+                           {name: "Doggy", force: 0.2},
+                           {name: "Kitty", force: 0.1},
+                           {name: "Mouse", force: 0.01}];
